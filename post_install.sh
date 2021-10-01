b6653f177e64b0d498a7ea97c67e2ca2d011949d
Allen Sandiego <allen.sandiego@gmail.com>
2021-10-01 20:30:21 +0800

    Updated post_install for debugging purposes

--- post_install.sh
+++ post_install.sh
@@ -1,22 +1,6 @@
 #!/bin/sh
 
-# Enable and start mysql service
-sysrc mysql_enable=YES && service mysql-server start
-
 # Download Shinobi repository and copy default config files
-git clone https://gitlab.com/Shinobi-Systems/Shinobi.git /usr/local/shinobi && cd /usr/local/shinobi && cp conf.sample.json conf.json && cp super.sample.json super.json
+git clone https://gitlab.com/Shinobi-Systems/Shinobi.git /usr/local/shinobi 
 
-# Change mysql root password
-mysql -h localhost -u root -e -p`tail -n +2 /root/.mysql_secret` --connect-expired-password <<EOF
-alter user 'root'@'localhost' identified by '`tail -n +2 /root/.mysql_secret`;
-EOF
-  
-# Create Shinobi user in mysql
-mysql -h localhost -u root -e -p`tail -n +2 /root/.mysql_secret` -e "source sql/user.sql"
-
-# Install Shinobi framework in mysql
-mysql -h localhost -u root -e -p`tail -n +2 /root/.mysql_secret` -e "source sql/framework.sql"
-
-# Install npm components and start shinobi
-npm install -g -save npm pm2@3.0.0 --unsafe-perm >> /tmp/npm-install.log && pm2 start camera.js cron.js && pm2 save && pm2 list && pm2 startup rcd
-
+echo "Done installing..."
