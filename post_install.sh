ac0008b8fcdeaf6d7a8845f2b4554508d7f41382
root <root@freenas.local>
2021-10-01 15:00:40 +0800

    Updated post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
 
-echo "Hello World"
-
-#cd /home
-#git clone https://gitlab.com/Shinobi-Systems/Shinobi.git shinobi
-#cd shinobi
-#sysrc mysql_enable=YES
-#service mysql-server start
-#mysql -h localhost -u root -e "source sql/user.sql"
-#mysql -h localhost -u root -e "source sql/framework.sql"
-#npm install -g npm
-#npm install --unsafe-perm
-#npm install -g pm2@latest
-#cp conf.sample.json conf.json
-#cp super.sample.json super.json
-#pm2 start camera.js
-#pm2 start cron.js
-#pm2 save
-#pm2 list
-#pm2 startup rcd
+cd /home
+git clone https://gitlab.com/Shinobi-Systems/Shinobi.git shinobi
+cd shinobi
+sysrc mysql_enable=YES
+service mysql-server start
+mysql -h localhost -u root -e "source sql/user.sql"
+mysql -h localhost -u root -e "source sql/framework.sql"
+npm install -g npm
+npm install --unsafe-perm
+npm install -g pm2@latest
+cp conf.sample.json conf.json
+cp super.sample.json super.json
+pm2 start camera.js
+pm2 start cron.js
+pm2 save
+pm2 list
+pm2 startup rcd
 
 
+echo "Download mobile app for android on https://peek.allensandiego.com/"
+echo "Disclaimer: Peek for Android is not in anyway connected to Shinobi or its developer/s."
+echo ""
