683392d83ed9d671ccc080bcbbc905c902215eda
Allen Sandiego <allen.sandiego@gmail.com>
2021-10-01 22:40:35 +0800

    Added mysql for debugging

--- post_install.sh
+++ post_install.sh
@@ -3,4 +3,17 @@
 # Download Shinobi repository and copy default config files
 git clone https://gitlab.com/Shinobi-Systems/Shinobi.git /usr/local/shinobi
 
+# Enable mysql service
+sysrc mysql_enable=YES 
+
+# Start mysql service
+service mysql-server start
+
+# Configure mysql database
+mysql -h localhost -u root -e -p`tail -n +2 /root/.mysql_secret` --connect-expired-password <<EOF
+alter user 'root'@'localhost' identified by '`tail -n +2 /root/.mysql_secret`;
+source /usr/local/shinobi/sql/user.sql
+source /usr/local/shinobi/sql/framework.sql
+EOF
+
 echo "Done installing..."
