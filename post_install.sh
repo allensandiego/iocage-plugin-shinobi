1bda50874db856716389a425b82ae9b9689313bc
Allen Sandiego <allen.sandiego@gmail.com>
2021-10-02 07:50:54 +0800

    More debugging codes

--- post_install.sh
+++ post_install.sh
@@ -10,10 +10,15 @@ sysrc mysql_enable=YES
 service mysql-server start
 
 # Configure mysql database
-mysql -h localhost -u root -e -p`tail -n +2 /root/.mysql_secret` --connect-expired-password <<EOF
+echo "mysql -h localhost -u root -e -p`tail -n +2 /root/.mysql_secret` --connect-expired-password <<EOF
 alter user 'root'@'localhost' identified by '`tail -n +2 /root/.mysql_secret`;
 source /usr/local/shinobi/sql/user.sql
 source /usr/local/shinobi/sql/framework.sql
 EOF
+" >> /tmp/mysql_init.sh
+
+cat /tmp/mysql_init.sh
+
+sh /tmp/mysql_init.sh
 
 echo "Done installing..."
