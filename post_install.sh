c4a31aef6b061f46b4be3cf006aea7809a06c67b
Allen Sandiego <allen.sandiego@gmail.com>
2021-10-01 09:07:58 +0800

    Updated post_install

--- post_install.sh
+++ post_install.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
 
-cd /home
-git clone https://gitlab.com/Shinobi-Systems/Shinobi.git shinobi
-cd shinobi
-sysrc mysql_enable=YES
-service mysql-server start
-mysql -h localhost -u root -e "source sql/user.sql"
-mysql -h localhost -u root -e "source sql/framework.sql"
-npm install -g npm
-npm install --unsafe-perm
-npm install -g pm2@latest
-cp conf.sample.json conf.json
-cp super.sample.json super.json
-pm2 start camera.js
-pm2 start cron.js
-pm2 save
-pm2 list
-pm2 startup rcd
+pwd
+
+#cd /home
+#git clone https://gitlab.com/Shinobi-Systems/Shinobi.git shinobi
+#cd shinobi
+#sysrc mysql_enable=YES
+#service mysql-server start
+#mysql -h localhost -u root -e "source sql/user.sql"
+#mysql -h localhost -u root -e "source sql/framework.sql"
+#npm install -g npm
+#npm install --unsafe-perm
+#npm install -g pm2@latest
+#cp conf.sample.json conf.json
+#cp super.sample.json super.json
+#pm2 start camera.js
+#pm2 start cron.js
+#pm2 save
+#pm2 list
+#pm2 startup rcd
 
 
