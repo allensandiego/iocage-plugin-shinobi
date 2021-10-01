2447392a73365fa9fa9cceea4737c48af6cefcf8
Allen Sandiego <allen.sandiego@gmail.com>
2021-10-01 20:36:01 +0800

    Updated post_install for debugging purposes

--- post_install.sh
+++ post_install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Download Shinobi repository and copy default config files
-git clone https://gitlab.com/Shinobi-Systems/Shinobi.git /usr/local/shinobi 
+git clone https://gitlab.com/Shinobi-Systems/Shinobi.git /usr/local/shinobi
 
 echo "Done installing..."
