70c46b74d554bb5ebc1350d7bd731effbd5ac9f6
Allen Sandiego <allen.sandiego@gmail.com>
2021-10-01 19:25:44 +0800

    Fixes on post_install

--- post_install.sh
+++ post_install.sh
@@ -6,18 +6,16 @@ sysrc mysql_enable=YES && service mysql-server start
 # Download Shinobi repository and copy default config files
 git clone https://gitlab.com/Shinobi-Systems/Shinobi.git /usr/local/shinobi && cd /usr/local/shinobi && cp conf.sample.json conf.json && cp super.sample.json super.json
 
-if [ -e "/root/.mysql_secret" ] ; then
-  # Change mysql root password
-  mysql -h localhost -u root -e -p`tail -n +2 /root/.mysql_secret` --connect-expired-password <<EOF
-  alter user 'root'@'localhost' identified by '`tail -n +2 /root/.mysql_secret`;
-  EOF
+# Change mysql root password
+mysql -h localhost -u root -e -p`tail -n +2 /root/.mysql_secret` --connect-expired-password <<EOF
+alter user 'root'@'localhost' identified by '`tail -n +2 /root/.mysql_secret`;
+EOF
   
-  # Create Shinobi user in mysql
-  mysql -h localhost -u root -e -p`tail -n +2 /root/.mysql_secret` -e "source sql/user.sql"
+# Create Shinobi user in mysql
+mysql -h localhost -u root -e -p`tail -n +2 /root/.mysql_secret` -e "source sql/user.sql"
 
-  # Install Shinobi framework in mysql
-  mysql -h localhost -u root -e -p`tail -n +2 /root/.mysql_secret` -e "source sql/framework.sql"
-fi
+# Install Shinobi framework in mysql
+mysql -h localhost -u root -e -p`tail -n +2 /root/.mysql_secret` -e "source sql/framework.sql"
 
 # Install npm components and start shinobi
 npm install -g -save npm pm2@3.0.0 --unsafe-perm >> /tmp/npm-install.log && pm2 start camera.js cron.js && pm2 save && pm2 list && pm2 startup rcd
