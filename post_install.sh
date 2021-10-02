34f428ca71f54a5c1fc2274b7576d45a187632bb
Allen Sandiego <allen.sandiego@gmail.com>
2021-10-02 23:10:20 +0800

    Updated post_install for Shinibo installation

--- post_install.sh
+++ post_install.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 # Download Shinobi repository and copy default config files
-git clone https://gitlab.com/Shinobi-Systems/Shinobi.git /usr/local/shinobi
+git clone https://gitlab.com/Shinobi-Systems/Shinobi.git /usr/local/shinobi && cd /usr/local/shinobi
+
+# Create default config files
+cp conf.sample.json conf.json
+cp super.sample.json super.json
 
 # Enable mysql service
 sysrc mysql_enable=YES 
@@ -9,16 +13,24 @@ sysrc mysql_enable=YES
 # Start mysql service
 service mysql-server start
 
-# Configure mysql database
-echo "mysql -h localhost -u root -p\"`tail -n +2 /root/.mysql_secret`\" --connect-expired-password <<EOF
+# Setup Shinobi mysql database
+mysql -h localhost -u root -p\"`tail -n +2 /root/.mysql_secret`\" --connect-expired-password <<EOF
 alter user 'root'@'localhost' identified by '`tail -n +2 /root/.mysql_secret`';
 source /usr/local/shinobi/sql/user.sql
 source /usr/local/shinobi/sql/framework.sql
 EOF
-" >> /tmp/mysql_init.sh
 
-cat /tmp/mysql_init.sh
+# Install npm components
+npm install -g npm
+npm install -g --unsafe-perm
+npm install -g pm2@3.0.0
 
-sh /tmp/mysql_init.sh
+# Start shinobi
+pm2 start camera.js 
+pm2 start cron.js 
+pm2 save 
+pm2 list 
+pm2 startup rcd
 
-echo "Done installing..."
+echo
+echo 
