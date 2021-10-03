9c036c8c328cae5e77255709f2a1be5625dddd65
Allen Sandiego <allen.sandiego@gmail.com>
2021-10-03 10:20:46 +0800

    Finalize script

--- post_install.sh
+++ post_install.sh
@@ -14,7 +14,7 @@ sysrc mysql_enable=YES
 service mysql-server start
 
 # Setup Shinobi mysql database
-mysql -h localhost -u root -p\"`tail -n +2 /root/.mysql_secret`\" --connect-expired-password <<EOF
+mysql -h localhost -u root -p"`tail -n +2 /root/.mysql_secret`" --connect-expired-password <<EOF
 alter user 'root'@'localhost' identified by '`tail -n +2 /root/.mysql_secret`';
 source /usr/local/shinobi/sql/user.sql
 source /usr/local/shinobi/sql/framework.sql
@@ -34,17 +34,17 @@ pm2 save
 pm2 list 
 pm2 startup rcd
 
-echo "=====================================" >> /root/PLUGIN_INFO
-echo "||=====   Install Completed   =====||" >> /root/PLUGIN_INFO
-echo "=====================================" >> /root/PLUGIN_INFO
-echo "||  Login with the Superuser and   ||" >> /root/PLUGIN_INFO
-echo "||       create a new user at      ||" >> /root/PLUGIN_INFO
-echo "||  http://THIS_JAIL_IP:8080/super ||" >> /root/PLUGIN_INFO
-echo "||===================================" >> /root/PLUGIN_INFO
-echo "|| Superuser : admin@shinobi.video ||" >> /root/PLUGIN_INFO
-echo "||     Default Password : admin    ||" >> /root/PLUGIN_INFO
-echo "=====================================" >> /root/PLUGIN_INFO
-echo "|| Also, check out the unofficial  ||" >> /root/PLUGIN_INFO
-echo "|| android app for Shinobi at      ||" >> /root/PLUGIN_INFO
-echo "|| https://peek.allensandiego.com/ ||" >> /root/PLUGIN_INFO
-echo "=====================================" >> /root/PLUGIN_INFO
+echo "Getting Started" >> /root/PLUGIN_INFO
+echo "" >> /root/PLUGIN_INFO
+echo "- Login with the default superuser account and create a new user at http://$IOCAGE_PLUGIN_IP:8080/super" >> /root/PLUGIN_INFO
+echo "" >> /root/PLUGIN_INFO
+echo "  username : admin@shinobi.video" >> /root/PLUGIN_INFO
+echo "  password : admin" >> /root/PLUGIN_INFO
+echo "" >> /root/PLUGIN_INFO
+echo "- After creating a new user, go to http://$IOCAGE_PLUGIN_IP:8080/ and login with the new user credentials" >> /root/PLUGIN_INFO
+echo "- For Shinobi documentation, go to https://shinobi.video/docs/" >> /root/PLUGIN_INFO
+echo "" >> /root/PLUGIN_INFO
+echo "P.S." >> /root/PLUGIN_INFO
+echo "" >> /root/PLUGIN_INFO
+echo "Check out the unofficial android app for Shinobi at https://peek.allensandiego.com/" >> /root/PLUGIN_INFO
+echo "" >> /root/PLUGIN_INFO
